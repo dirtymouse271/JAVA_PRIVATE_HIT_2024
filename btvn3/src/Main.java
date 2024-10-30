f3e573f8e665c7b92a400165272a9d125bfc985e
dirtymouse271 <cheetahspeedyv@gmail.com>
2024-10-30 21:04:21 +0700

    btvn3

---
+++ btvn3/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.setStudentCode(2023600784);
+        student1.setFullName("Pham Duc Thanh");
+        student1.setAge(19);
+        student1.setPhoneNumber("0376616774");
+        student1.setEmail("cheetahspeedyv@gmail.com");
+        student1.display();
+    }
+}
